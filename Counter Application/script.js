1ad514e6b4080b464b1dceda3f534723e79f4332
Yashwanth1124 <120181936+Yashwanth1124@users.noreply.github.com>
2023-08-01 19:11:41 +0530

    Add files via upload

---
+++ Counter Application/script.js
@@ -0,0 +1,44 @@
+let counterElement = document.getElementById("counterValue");
+
+function onDecrement(){
+    let previousCounterValue = counterElement.textContent;
+    let updatedCounterValue = parseInt(previousCounterValue) - 1;
+    counterElement.textContent = updatedCounterValue
+    if (updatedCounterValue > 0){
+        counterElement.style.color = "green"
+    }
+    else if(updatedCounterValue<0){
+        counterElement.style.color = "red"
+    }
+    else{
+        counterElement.style.color = "black"
+    }
+}
+function onIncrement(){
+    let previousCounterValue = counterElement.textContent;
+    let updatedCounterValue = parseInt(previousCounterValue) + 1;
+    counterElement.textContent = updatedCounterValue
+    if (updatedCounterValue>0){
+        counterElement.style.color = "green"
+    }
+    else if(updatedCounterValue<0){
+        counterElement.style.color = "red"
+    }
+    else{
+        counterElement.style.color = "black"
+    }
+
+}
+function onReset(){
+    let updatedCounterValue = 0;
+    counterElement.textContent = parseInt(updatedCounterValue)
+    if (updatedCounterValue>0){
+        counterElement.style.color = "green"
+    }
+    else if(updatedCounterValue<0){
+        counterElement.style.color = "red"
+    }
+    else{
+        counterElement.style.color = "black"
+    }
+}
